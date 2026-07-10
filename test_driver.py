69bab39deaa9aa5b91785848d23c706f3c524f58
jecblood <85044167+jecblood@users.noreply.github.com>
2026-07-10 14:28:22 -0400

    Replace sensitive database credentials with placeholders
    Updated connection string placeholders for database access.

--- test_driver.py
+++ test_driver.py
@@ -5,10 +5,10 @@ print(pyodbc.drivers())
 
 conn_str = (
     "DRIVER={ODBC Driver 18 for SQL Server};"
-    "SERVER=jeff-geo-sql-server.database.windows.net;"
-    "DATABASE=geo_metadata_db;"
-    "UID=jecblood;"
-    "PWD=Jfdnfn11!;"
+    "SERVER=SERVER;"
+    "DATABASE=DATABASE_NAME;"
+    "UID=YOUR_USERNAME;"
+    "PWD=YOUR_PASSWORD;"
     "Encrypt=yes;"
     "TrustServerCertificate=no;"
 )
